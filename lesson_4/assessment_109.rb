5f83fd79abf4b260070f10822e215a3d70ec7198
Brandon Brown <Brandonbrownb2@Gmail.com>
2016-02-07 16:39:17 +0700

    Add new file

---
+++ lesson_4/assessment_109.rb
@@ -0,0 +1,41 @@
+# agenda
+
+# 1.) Communication
+#     - style / tone
+#     - vocabulary, use the right terms
+# 2.) Ruby Basics
+#     - Local Variable Scope
+#     - Method Args
+#     - How Methods work, mutatation/re-assignment
+#     - Collections, most commonly used.
+#     - Blocks
+# 3.) Problem Solving
+#     - small exercises
+
+# 1) focus on your vocabulary. Call things by their proper and 
+# right terms. For example, don't say "that thing returns nothing"; 
+# say "that method returns nil". There's no such thing as "nothing". 
+# Don't say "the results of that is 5", 
+# say "the return value of that block is the integer 5". 
+# Don't say "x = 2 is true", say "x = 3 is truthy, or evaluates to true". 
+# Precision of language is paramount, and the assessment will be 
+# looking for that.
+
+# 2) focus on the 3 concepts you mentioned in your email below. 
+#  with the code and tease out edge cases. 
+#  There's depth even in those 3 little concepts. 
+#  Don't try to master everything right now, but really hone in on 
+#  those 3 concepts. 
+
+# 3) study and practice. Our best students are like valedictorians 
+# in high school. They take this seriously; they take notes, 
+# make flashcards, practice and rehearse, etc. 
+# You should too if you want to compete with them. 
+
+n = 10
+
+1.times do |num|
+  n = 11
+end
+
+puts n
