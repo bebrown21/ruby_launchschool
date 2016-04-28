87233079386c0bc0286206fe92c7746b7d33834b
Brandon Brown <Brandonbrownb2@Gmail.com>
2016-04-28 11:18:14 +0700

    finalize project files

--- web_development/cms/cms.rb
+++ web_development/cms/cms.rb
@@ -1,21 +1,123 @@
-# cms.rb
-require 'sinatra'
-require 'sinatra/reloader'
-require 'tilt/erubis'
+require "sinatra"
+require "sinatra/reloader"
+require "tilt/erubis"
+require "redcarpet"
+require "yaml"
 
-root = File.expand_path("..", __FILE__)
-
-get '/' do
-  @files = Dir.glob(root + "/data/*").map do |path|
-    File.basename(path)
-  end
-
-erb :index
+configure do
+  enable :sessions
+  set :session_secret, 'super secret'
 end
 
-get '/:filename' do
-  file_path = root + "/data/" + params[:filename]
+def data_path
+  if ENV["RACK_ENV"] == "test"
+    File.expand_path("../test/data", __FILE__)
+  else
+    File.expand_path("../data", __FILE__)
+  end
+end
 
-  headers["Content-Type"] = "text/plain"
-  File.read(file_path)
+def render_markdown(text)
+  markdown = Redcarpet::Markdown.new(Redcarpet::Render::HTML)
+  markdown.render(text)
+end
+
+def load_file_content(path)
+  content = File.read(path)
+  case File.extname(path)
+  when ".txt"
+    headers["Content-Type"] = "text/plain"
+    content
+  when ".md"
+    erb render_markdown(content)
+  end
+end
+
+get "/" do
+  pattern = File.join(data_path, "*")
+  @files = Dir.glob(pattern).map do |path|
+    File.basename(path)
+  end
+  erb :index
+end
+
+get "/:filename" do
+  file_path = File.join(data_path, params[:filename])
+
+  if File.exist?(file_path)
+    load_file_content(file_path)
+  else
+    session[:message] = "#{params[:filename]} does not exist."
+    redirect "/"
+  end
+end
+
+get "/:filename/edit" do
+  file_path = File.join(data_path, params[:filename])
+
+  @filename = params[:filename]
+  @content = File.read(file_path)
+
+  erb :edit
+end
+
+post "/:filename" do
+  file_path = File.join(data_path, params[:filename])
+
+  File.write(file_path, params[:content])
+
+  session[:message] = "#{params[:filename]} has been updated."
+  redirect "/"
+end
+
+get "/new" do
+  erb :new
+end
+
+post "/create" do
+  filename = params[:filename].to_s
+
+  if filename.size == 0
+    session[:message] = "A name is required."
+    status 422
+    erb :new
+  else
+    file_path = File.join(data_path, filename)
+
+    File.write(file_path, "")
+    session[:message] = "#{params[:filename]} has been created."
+
+    redirect "/"
+  end
+end
+
+post "/:filename/delete" do
+  file_path = File.join(data_path, params[:filename])
+
+  File.delete(file_path)
+
+  session[:message] = "#{params[:filename]} has been deleted."
+  redirect "/"
+end
+
+get "/users/signin" do
+  erb :signin
+end
+
+post "/users/signin" do
+  if params[:username] == "admin" && params[:password] == "secret"
+    session[:username] = params[:username]
+    session[:message] = "Welcome!"
+    redirect "/"
+  else
+    session[:message] = "Invalid credentials"
+    status 422
+    erb :signin
+  end
+end
+
+post "/users/signout" do
+  session.delete(:username)
+  session[:message] = "You have been signed out."
+  redirect "/"
 end
